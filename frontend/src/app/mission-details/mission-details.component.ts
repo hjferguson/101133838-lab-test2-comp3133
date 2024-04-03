636aca484df9ecc05fe249729f64970e9bd2c878
hjferguson <harlanjazz@gmail.com>
2024-04-03 17:40:23 -0400

    adds direct video embedding into detailed flight

--- frontend/src/app/mission-details/mission-details.component.ts
+++ frontend/src/app/mission-details/mission-details.component.ts
@@ -4,6 +4,7 @@ import { SpacexService } from '../spacex.service';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { NgClass } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-mission-details',
@@ -14,10 +15,12 @@ import { NgClass } from '@angular/common';
 })
 export class MissionDetailsComponent implements OnInit {
   missionDetails: any;
+  videoUrl: SafeResourceUrl | undefined;
 
   constructor(
     private route: ActivatedRoute,
-    private spacexService: SpacexService
+    private spacexService: SpacexService,
+    private sanitizer: DomSanitizer
   ) {}
 
   ngOnInit() {
@@ -29,10 +32,25 @@ export class MissionDetailsComponent implements OnInit {
     });
   }
 
+  getYouTubeVideoId(url: string): string | null {
+    const regExp = /^.*(youtu\.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
+    const match = url.match(regExp);
+  
+    return (match && match[2].length === 11) ? match[2] : null;
+  }
+
   loadMissionDetails(flightNumber: string) {
     this.spacexService.getMissionDetails(flightNumber).subscribe({
       next: (data: any) => {
         this.missionDetails = data;
+        if(this.missionDetails.links && this.missionDetails.links.video_link) {
+          const videoId = this.getYouTubeVideoId(this.missionDetails.links.video_link);
+          const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+          this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
+        }
+        
+        
+        
       },
       error: (error: any) => {
         console.error('Error fetching mission details:', error);
